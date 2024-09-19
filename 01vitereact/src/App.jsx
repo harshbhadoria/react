f1a94e4e2f64702229cbc8a60a82c9e3a45c841a
harshbhadoria <abhi.bhadoria123@gmail.com>
2024-09-19 19:38:01 +0530

    To be

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -4,7 +4,12 @@ import Chai from "./chai"
 function App() {
 
   return (
+    <>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, obcaecati?
+    </p>
     <Chai/>
+    <h4>Raju Naan</h4>
+    </>
   )
 }
 
